9ae966e54719d4bcf35a1ff8d5177794611b8d85
jose <jgijalm388@g.educaand.es>
2026-01-12 19:24:16 +0100

    refactor: use TYPE_COLORS for dynamic styling of Pokemon types in DataCard

--- src/components/DataCard.tsx
+++ src/components/DataCard.tsx
@@ -4,6 +4,28 @@ interface Props {
     pokemon: PokemonWithDetails;
 }
 
+const TYPE_COLORS: Record<string, string> = {
+    normal: "bg-gray-400",
+    fire: 'bg-red-500',
+    water: 'bg-blue-500',
+    grass: 'bg-green-500',
+    electric: 'bg-yellow-400',
+    ice: 'bg-cyan-300',
+    fighting: 'bg-orange-600',
+    poison: 'bg-purple-500',
+    ground: 'bg-yellow-700',
+    flying: 'bg-indigo-400',
+    psychic: 'bg-pink-500',
+    bug: 'bg-lime-500',
+    rock: 'bg-stone-500',
+    ghost: 'bg-violet-700',
+    dragon: 'bg-indigo-700',
+    dark: 'bg-gray-800',
+    steel: 'bg-slate-400',
+    fairy: 'bg-pink-300',
+};
+
+
 export const DataCard = ({ pokemon }: Props) => {
     return (
         <li
@@ -32,7 +54,7 @@ export const DataCard = ({ pokemon }: Props) => {
                 {pokemon.types.map((type) => (
                     <span
                         key={type}
-                        className="bg-red-500 rounded-2xl px-3 py-2"
+                        className={`rounded-2xl px-3 py-2 ${TYPE_COLORS[type]}`}
                     >
                         {type}
                     </span>
